b522d0ecf451809b0ad462212199d81b3df93b2f
Imad Ullah <35524866+ImadUllah@users.noreply.github.com>
2020-12-29 20:00:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Bla bla bla
+          Edit <code>src/App.js</code> and save to reload. Enjoying DevOps
         </p>
         <a
           className="App-link"
